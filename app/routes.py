71ae239f57dacdcc316afaef030a4d7b9ac969ff
coconnell1 <coconnell1@ithaca.edu>
2018-09-11 13:09:59 -0400

    lab 1 - hip hop artists

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,46 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+def index():
+    user = {'username': 'Cobi'}
+
+    return render_template('/index.html', title="Home", user=user)
+
+@app.route('/artistList')
+def artist_list():
+    artists = [
+        {
+            'artist': 'Kendrick Lamar'
+        },
+        {
+            'artist': 'Tupac'
+        },
+        {
+            'artist': 'Nas'
+        },
+        {
+            'artist': 'Eminem'
+        },
+        {
+            'artist': 'Joey Bada$$'
+        },
+        {
+            'artist': 'XXXTentacion'
+        },
+        {
+            'artist': 'Childish Gambino'
+        }
+        ]
+
+    return render_template('/artistList.html', title="List of Artists", artists=artists)
+
+@app.route('/artistPage')
+def artist_page():
+
+    return render_template('/artistPage.html', title="Artist Page")
+
+@app.route('/createArtist')
+def create_artist():
+
+    return render_template('/createArtist.html', title="Create New Artist")
 
