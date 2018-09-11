b35930ae88fd30547870f1d773a1aeda9ee66f5f
coconnell1 <coconnell1@ithaca.edu>
2018-09-11 14:09:39 -0400

    lab 2 - hip hop artists (COMPLETED)

--- app/routes.py
+++ app/routes.py
@@ -38,8 +38,18 @@ def artist_list():
 
 @app.route('/artistPage')
 def artist_page():
+    info = [
+        {
+            'artistInfo': 'Kendrick Lamar',
+            'birth': 'Kendrick Lamar was born on June 17th, 1987 in Compton, California.',
+            'music': 'He is known to be one of the best lyricists, and story-tellers in all of hip-hop.',
+            'concert': ' He is currently on tour, and his next concert is on September 14th in Denver, Colorado.'
 
-    return render_template('/artistPage.html', title="Artist Page")
+        }
+
+    ]
+
+    return render_template('/artistPage.html', title="Artist Page", info=info)
 
 @app.route('/createArtist')
 def create_artist():
